04902b67e958f90477a96deb623c3b461bb89ad0
James Lemieux <James Lemieux>
2016-08-31 17:15:35 -0400

    added current schema

--- schema.sql
+++ schema.sql
@@ -1,30 +1,29 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.5.42)
-# Database: sa
-# Generation Time: 2016-08-29 18:35:26 +0000
-# ************************************************************
-
+CREATE DATABASE  IF NOT EXISTS `stocks` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `stocks`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: stocks
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.16-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
-# Dump of table quotes
-# ------------------------------------------------------------
+--
+-- Table structure for table `quotes`
+--
 
 DROP TABLE IF EXISTS `quotes`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `quotes` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `ask` float NOT NULL,
@@ -32,50 +31,55 @@ CREATE TABLE `quotes` (
   `bid` float NOT NULL,
   `bidvolume` int(11) NOT NULL,
   `symbol_id` int(11) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `timestamp` bigint(15) NOT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `symbol_id_UNIQUE` (`symbol_id`),
   KEY `symbol` (`symbol_id`),
   KEY `timestamp` (`timestamp`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=259 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
-# Dump of table symbols
-# ------------------------------------------------------------
+--
+-- Table structure for table `symbols`
+--
 
 DROP TABLE IF EXISTS `symbols`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `symbols` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `symbol` varchar(10) NOT NULL DEFAULT '',
-  `created_at` int(11) DEFAULT NULL,
+  `created_at` timestamp(4) NULL DEFAULT CURRENT_TIMESTAMP(4),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
-# Dump of table trades
-# ------------------------------------------------------------
+--
+-- Table structure for table `trades`
+--
 
 DROP TABLE IF EXISTS `trades`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `trades` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `last` float NOT NULL,
   `symbol_id` int(11) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `timestamp` bigint(15) NOT NULL DEFAULT '0',
   `tradedvolume` int(11) NOT NULL,
   `vwa` float NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
-# Dump of table watchlist
-# ------------------------------------------------------------
+--
+-- Table structure for table `watchlist`
+--
 
 DROP TABLE IF EXISTS `watchlist`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `watchlist` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL DEFAULT '',
@@ -83,14 +87,15 @@ CREATE TABLE `watchlist` (
   PRIMARY KEY (`id`),
   KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
-# Dump of table watchlist_options
-# ------------------------------------------------------------
+--
+-- Table structure for table `watchlist_options`
+--
 
 DROP TABLE IF EXISTS `watchlist_options`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `watchlist_options` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `watchlist_id` int(11) NOT NULL,
@@ -99,14 +104,15 @@ CREATE TABLE `watchlist_options` (
   PRIMARY KEY (`id`),
   KEY `watchlist_id` (`watchlist_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
-# Dump of table watchlist_symbols
-# ------------------------------------------------------------
+--
+-- Table structure for table `watchlist_symbols`
+--
 
 DROP TABLE IF EXISTS `watchlist_symbols`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `watchlist_symbols` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `symbol_id` int(11) NOT NULL,
@@ -116,13 +122,15 @@ CREATE TABLE `watchlist_symbols` (
   KEY `symbol_id` (`symbol_id`),
   KEY `watchlist_id` (`watchlist_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-08-31 17:14:55
